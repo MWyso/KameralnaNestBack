b2067ded0e3d9ca364b7cc6e5a3766177868ffc8
WysoPC <mateusz.wyso94@gmail.com>
2023-07-07 14:08:45 +0200

    config BE

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { forwardRef, Module } from '@nestjs/common';
+// import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ScheduleModule } from '@nestjs/schedule';
+import { DatabaseConfiguration } from './config/typeorm.config';
+import { AdminModule } from './admin/admin.module';
+// import { AtGuard } from './common/guards';
+// import { CronService } from './cron/cron.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfiguration,
+    }),
+    CacheModule.register({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 5,
+    }),
+    forwardRef(() => AdminModule),
+  ],
+  // providers: [{ provide: APP_GUARD, useClass: AtGuard }, CronService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  // app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'localhost:3000',
+    credentials: true,
+  });
 
+  await app.listen(
+    configService.get('PORT') ? parseInt(configService.get('PORT')) : 3000,
+  );
 }
-bootstrap();
 
+bootstrap();
