4670e495f3a311e3cf58ede312c3507ce4779846
WysoPC <mateusz.wyso94@gmail.com>
2023-07-10 09:53:57 +0200

    config be, types, running test db

---
+++ src/admin/entity/admin.entity.ts
@@ -0,0 +1,42 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserRole } from '@Types';
+
+@Entity()
+export class Admin extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Column({ length: 255 })
+  email: string;
+
+  @Column({ length: 255 })
+  password: string;
+
+  @Column({ type: 'enum', default: UserRole.ADMIN, enum: UserRole })
+  role: UserRole;
+
+  @Column({ default: '', length: 255, nullable: true })
+  refreshToken: string;
+
+  @Column({ default: '', length: 255, nullable: true })
+  verificationToken: string;
+
+  @Column({ default: '', length: 255 })
+  activationUrl: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
