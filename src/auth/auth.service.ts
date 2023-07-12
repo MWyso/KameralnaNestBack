e6be053d5fd79db447cc216c433eb8c05bc2ec05
WysoPC <mateusz.wyso94@gmail.com>
2023-07-12 10:42:56 +0200

    passport-jwt, tokens strategy, jwtpayload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { AdminService } from "../admin/admin.service";
-import { CACHE_MANAGER } from "@nestjs/common/cache";
 import { UsersService } from "../users/users.service";
 import * as bcryptjs from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
@@ -9,7 +8,6 @@ import { JwtService } from '@nestjs/jwt';
 export class AuthService {
 
   constructor(
-    // @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private adminService: AdminService,
     private usersService: UsersService,
     // private jwtService: JwtService,
