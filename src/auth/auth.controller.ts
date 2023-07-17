498f7a8d6c17004c08b0d3ba3cd7c29d0accc903
WysoPC <mateusz.wyso94@gmail.com>
2023-07-17 14:01:22 +0200

    login/logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Response } from 'express';
+import { LoginUserDto } from "./dto";
+import { ApiResponse, UserDataResponse } from "@Types";
+import { GetUserId, Public } from "../common/decorators";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {
+  }
+
+  @Public()
+  @Post('/login')
+  async login(
+    @Body() loginData: LoginUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<ApiResponse<UserDataResponse>> {
+    return await this.authService.login(loginData, response);
+  }
+
+  @Post('/logout')
+  logout(@GetUserId() id: string, @Res({ passthrough: true }) res: Response): Promise<any> {
+    return this.authService.logout(id, res);
+  }
+}
